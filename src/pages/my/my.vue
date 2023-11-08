e8476038a32b11ff8bf23f7ca283386df2e7284c
L2233 <760142315@qq.com>
2023-11-08 23:05:15 +0800

    feat：请求封装

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { getHomeBannerAPI } from '@/services/home'
 
 const memberStore = useMemberStore()
+const getData = async () => {
+  const res = await getHomeBannerAPI()
+}
 </script>
 
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-        })
-      "
-      size="mini"
-      plain
-      type="primary"
-    >
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋',
+      })
+      " size="mini" plain type="primary">
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData()" type="primary" size="mini" plain>获取信息</button>
   </view>
 </template>
 
