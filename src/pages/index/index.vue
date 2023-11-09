301828f1b6db4b436c2cda20ae633e24f63ceb29
L2233 <760142315@qq.com>
2023-11-10 00:28:05 +0800

    feat：首页轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import type { BannerItem } from '@/types/home';
 import CustomNavbar from './components/CustomNavbar.vue'
+import { ref } from 'vue';
+import { getHomeBannerAPI } from '@/services/home';
+import { onLoad } from '@dcloudio/uni-app';
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 页面加载
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
