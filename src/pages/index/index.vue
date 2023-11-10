9fdd49f9bde7f31f67bac43481826530478797fb
L2233 <760142315@qq.com>
2023-11-11 01:22:24 +0800

    feat：首页-猜你喜欢-分页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,6 +6,7 @@ import HotPanel from './components/HotPanel.vue'
 import { ref } from 'vue';
 import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home';
 import { onLoad } from '@dcloudio/uni-app';
+import type { XtxGuessInstance } from '@/types/component';
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -28,6 +29,14 @@ const getHomeHotData = async () => {
   hotList.value = res.result
 }
 
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
+
 // 页面加载
 onLoad(() => {
   getHomeBannerData()
@@ -39,7 +48,7 @@ onLoad(() => {
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <scroll-view scroll-y class="scroll-view">
+  <scroll-view scroll-y class="scroll-view" @scrolltolower="onScrolltolower">
     <!-- 自定义轮播图 -->
     <XtxSwiper :list="bannerList" />
     <!-- 分类面板 -->
@@ -47,7 +56,7 @@ onLoad(() => {
     <!-- 热门推荐 -->
     <HotPanel :list="hotList" />
     <!-- 猜你喜欢 -->
-    <XtxGuess />
+    <XtxGuess ref="guessRef" />
   </scroll-view>
 </template>
 
