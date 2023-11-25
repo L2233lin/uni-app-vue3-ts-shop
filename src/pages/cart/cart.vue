a3567d743aa5ad8c1d19dbbe88546da7efd9b128
L2233 <760142315@qq.com>
2023-11-25 21:43:44 +0800

    feat: 购物车模块-修改单品数量

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { deleteMemberCartAPI, getMemberCartAPI } from '@/services/cart'
+import { deleteMemberCartAPI, getMemberCartAPI, putMemberCartBySkuIdAPI } from '@/services/cart'
 import { useMemberStore } from '@/stores'
 import type { CartItem } from '@/types/cart'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import type { InputNumberBoxEvent } from '@/components/vk-data-input-number-box/vk-data-input-number-box'
 
 // 获取会员Store
 const memberStore = useMemberStore()
@@ -38,6 +39,11 @@ const onDeleteCart = (skuId: string) => {
     },
   })
 }
+
+// 修改商品数量
+const onChangeCount = (ev: InputNumberBoxEvent) => {
+  putMemberCartBySkuIdAPI(ev.index, { count: ev.value })
+}
 </script>
 
 <template>
@@ -69,9 +75,8 @@ const onDeleteCart = (skuId: string) => {
               </navigator>
               <!-- 商品数量 -->
               <view class="count">
-                <text class="text">-</text>
-                <input class="input" type="number" :value="item.count.toString()" />
-                <text class="text">+</text>
+                <vk-data-input-number-box v-model="item.count" :min="1" :max="item.stock" :index="item.skuId"
+                  @change="onChangeCount" />
               </view>
             </view>
             <!-- 右侧删除按钮 -->
