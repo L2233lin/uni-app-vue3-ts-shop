484c9297a9f895c14fcc45ab54590dd5efc7792a
L2233 <760142315@qq.com>
2023-11-29 13:20:48 +0800

    feat：订单详情-骨架屏

---
+++ src/pagesOrder/detail/components/PageSkeleton.vue
@@ -0,0 +1,314 @@
+<template name="skeleton">
+    <view class="sk-container">
+        <scroll-view :enable-back-to-top="true" :scroll-y="true" class="viewport" id="scroller">
+            <view class="overview sk-image" style="padding-top:64px">
+                <view class="status icon-clock sk-transparent sk-text-0-0000-218 sk-text sk-pseudo sk-pseudo-circle">等待付款
+                </view>
+                <view class="tips sk-transparent">
+                    <text class="money sk-transparent sk-text-0-0000-944 sk-text">应付金额: ¥ 99.00</text>
+                    <text class="time sk-transparent sk-text-0-0000-68 sk-text">支付剩余</text> 00 时 29 分 59 秒
+                </view>
+                <view class="button sk-transparent sk-text-28-1250-707 sk-text" style="background-position-x: 50%;">去支付
+                </view>
+            </view>
+            <view class="shipment">
+                <view class="item sk-image">
+                    <view class="message sk-transparent sk-text-14-2857-699 sk-text"> 您已在广州市天河区黑马程序员完成取件，感谢使用菜鸟驿站，期待再次为您服务。
+                    </view>
+                    <view class="date sk-transparent sk-text-14-2857-184 sk-text"> 2023-04-14 13:14:20 </view>
+                </view>
+                <view class="locate sk-image">
+                    <view class="user sk-transparent sk-text-14-2857-250 sk-text"> 张三 13333333333 </view>
+                    <view class="address sk-transparent sk-text-14-2857-684 sk-text"> 广东省 广州市 天河区 黑马程序员 </view>
+                </view>
+            </view>
+            <view class="goods">
+                <view class="item">
+                    <navigator class="navigator" hover-class="none">
+                        <image class="cover sk-image"></image>
+                        <view class="meta">
+                            <view class="name ellipsis sk-transparent sk-text-14-2857-458 sk-text">ins风小碎花泡泡袖衬110-160cm
+                            </view>
+                            <view class="type sk-transparent sk-text-22-2222-256 sk-text">藏青小花， 130</view>
+                            <view class="price">
+                                <view class="actual">
+                                    <text class="symbol sk-transparent sk-opacity">¥</text>
+                                    <text class="sk-transparent sk-text-14-2857-495 sk-text">99.00</text>
+                                </view>
+                            </view>
+                            <view class="quantity sk-transparent sk-opacity">x1</view>
+                        </view>
+                    </navigator>
+                    <navigator class="navigator" hover-class="none">
+                        <image class="cover sk-image"></image>
+                        <view class="meta">
+                            <view class="name ellipsis sk-transparent sk-text-14-2857-224 sk-text">ins风小碎花泡泡袖衬110-160cm
+                            </view>
+                            <view class="type sk-transparent sk-text-22-2222-259 sk-text">藏青小花， 130</view>
+                            <view class="price">
+                                <view class="actual">
+                                    <text class="symbol sk-transparent sk-opacity">¥</text>
+                                    <text class="sk-transparent sk-text-14-2857-862 sk-text">99.00</text>
+                                </view>
+                            </view>
+                            <view class="quantity sk-transparent sk-opacity">x1</view>
+                        </view>
+                    </navigator>
+                    <view class="action">
+                        <view class="button primary sk-transparent sk-text-28-3333-813 sk-text"
+                            style="background-position-x: 50%;">申请售后</view>
+                        <navigator class="button sk-transparent sk-text-28-3333-34 sk-text"
+                            style="background-position-x: 50%;"> 去评价 </navigator>
+                    </view>
+                </view>
+                <view class="total">
+                    <view class="row">
+                        <view class="text sk-transparent sk-text-0-0000-437 sk-text">商品总价: </view>
+                        <view class="symbol sk-transparent sk-text-0-0000-173 sk-text sk-pseudo sk-pseudo-circle">99.00
+                        </view>
+                    </view>
+                    <view class="row">
+                        <view class="text sk-transparent sk-text-0-0000-966 sk-text">运费: </view>
+                        <view class="symbol sk-transparent sk-text-0-0000-983 sk-text sk-pseudo sk-pseudo-circle">10.00
+                        </view>
+                    </view>
+                    <view class="row">
+                        <view class="text sk-transparent sk-text-0-0000-567 sk-text">应付金额: </view>
+                        <view class="symbol primary sk-transparent sk-text-0-0000-121 sk-text sk-pseudo sk-pseudo-circle">
+                            109.00</view>
+                    </view>
+                </view>
+            </view>
+            <view class="detail">
+                <view class="title sk-transparent sk-text-0-0000-82 sk-text">订单信息</view>
+                <view class="row">
+                    <view class="item sk-transparent"> 订单编号: 1729721931583721474
+                        <text class="copy sk-transparent sk-text-0-0000-152 sk-text">复制</text>
+                    </view>
+                    <view class="item sk-transparent sk-text-0-0000-233 sk-text">下单时间: 2023-04-14 13:14:20</view>
+                </view>
+            </view>
+            <view class="toolbar" style="padding-bottom:34px">
+                <view class="button primary sk-transparent sk-text-14-2857-741 sk-text"> 去支付 </view>
+                <view class="button sk-transparent sk-text-14-2857-9 sk-text"> 取消订单 </view>
+            </view>
+        </scroll-view>
+    </view>
+</template>
+<style>
+.sk-transparent {
+    color: transparent !important;
+}
+
+.sk-text-14-2857-965 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 44.8000rpx;
+    position: relative !important;
+}
+
+.sk-text {
+    background-origin: content-box !important;
+    background-clip: content-box !important;
+    background-color: transparent !important;
+    color: transparent !important;
+    background-repeat: repeat-y !important;
+}
+
+.sk-text-0-0000-218 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 36.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-944 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 28.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-68 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 28.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-28-1250-707 {
+    background-image: linear-gradient(transparent 28.1250%, #EEEEEE 0%, #EEEEEE 71.8750%, transparent 0%) !important;
+    background-size: 100% 64.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-699 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-184 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 33.6000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-250 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-684 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 33.6000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-458 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-text-22-2222-256 {
+    background-image: linear-gradient(transparent 22.2222%, #EEEEEE 0%, #EEEEEE 77.7778%, transparent 0%) !important;
+    background-size: 100% 43.2000rpx;
+    position: relative !important;
+}
+
+.sk-opacity {
+    opacity: 0 !important;
+}
+
+.sk-text-14-2857-495 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 33.6000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-224 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-text-22-2222-259 {
+    background-image: linear-gradient(transparent 22.2222%, #EEEEEE 0%, #EEEEEE 77.7778%, transparent 0%) !important;
+    background-size: 100% 43.2000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-862 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 33.6000rpx;
+    position: relative !important;
+}
+
+.sk-text-28-3333-813 {
+    background-image: linear-gradient(transparent 28.3333%, #EEEEEE 0%, #EEEEEE 71.6667%, transparent 0%) !important;
+    background-size: 100% 60.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-28-3333-34 {
+    background-image: linear-gradient(transparent 28.3333%, #EEEEEE 0%, #EEEEEE 71.6667%, transparent 0%) !important;
+    background-size: 100% 60.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-437 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-173 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-966 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-983 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-567 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-121 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 36.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-82 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 30.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-152 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 20.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-0-0000-233 {
+    background-image: linear-gradient(transparent 0.0000%, #EEEEEE 0%, #EEEEEE 100.0000%, transparent 0%) !important;
+    background-size: 100% 26.0000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-741 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-text-14-2857-9 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 36.4000rpx;
+    position: relative !important;
+}
+
+.sk-image {
+    background: #EFEFEF !important;
+}
+
+.sk-pseudo::before,
+.sk-pseudo::after {
+    background: #EFEFEF !important;
+    background-image: none !important;
+    color: transparent !important;
+    border-color: transparent !important;
+}
+
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+    border-radius: 0 !important;
+}
+
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+    border-radius: 50% !important;
+}
+
+.sk-container {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: transparent;
+}
+</style>
--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -4,7 +4,8 @@ import { getMemberOrderByIdAPI } from '@/services/order';
 import type { OrderResult } from '@/types/order';
 import { onLoad, onReady } from '@dcloudio/uni-app';
 import { ref } from 'vue';
-import { OrderState, orderStateList } from '@/utils/constants'
+import { OrderState, orderStateList } from '@/utils/constants';
+import PageSkeleton from './components/PageSkeleton.vue'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
