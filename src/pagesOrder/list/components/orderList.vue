5bc0c7d5b4738d328306e90618b6f2510bca75f1
L2233 <760142315@qq.com>
2023-12-01 03:05:00 +0800

    feat：订单列表-下拉刷新

--- src/pagesOrder/list/components/orderList.vue
+++ src/pagesOrder/list/components/orderList.vue
@@ -86,9 +86,33 @@ const onOrderDelete = (id: string) => {
     },
   })
 }
+
+// 是否分页结束
+const isFinish = ref(false)
+// 是否触发下拉刷新
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开始动画
+  isTriggered.value = true
+  // 重置数据
+  queryParams.page = 1
+  orderList.value = []
+  isFinish.value = false
+  // 加载数据
+  await getMemberOrderData()
+  // 关闭动画
+  isTriggered.value = false
+}
 </script>
 <template>
-  <scroll-view scroll-y class="orders">
+  <scroll-view
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    :refresher-triggered="isTriggered"
+    scroll-y
+    class="orders"
+  >
     <view class="card" v-for="order in orderList" :key="order.id">
       <!-- 订单信息 -->
       <view class="status">
